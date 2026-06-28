c19bb44741bebd62f3a2c50295d19046fbb5a77a
Matt <matthewpaulh@hotmail.co.uk>
2026-06-28 11:28:51 +0000

    fixed syntax errors

--- django_api/django_api/urls.py
+++ django_api/django_api/urls.py
@@ -18,12 +18,13 @@ from django.contrib import admin
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
+from django.http import HttpResponse
 
 urlpatterns = [
-    path('health/', lambda request: HttpResponse(status=204))),
+    path('health/', lambda request: HttpResponse(status=204)),
 ]
 
-if setting.STATIC_URL == "/static/":
-    urlpatterns.append(
+if settings.STATIC_URL == "/static/":
+    urlpatterns.extend(
         static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
     )
