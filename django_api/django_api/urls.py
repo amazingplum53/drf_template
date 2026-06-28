54cdcfe02790ef4d0e4ae951885dedcb175185ad
Matt <matthewpaulh@hotmail.co.uk>
2026-06-28 10:57:23 +0000

    added health check

--- django_api/django_api/urls.py
+++ django_api/django_api/urls.py
@@ -16,7 +16,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('health/', lambda request: HttpResponse(status=204))),
 ]
+
+if setting.STATIC_URL == "/static/":
+    urlpatterns.append(
+        static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    )
