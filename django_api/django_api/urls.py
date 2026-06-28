768881a0ffdb79d4edf45d6a30532364ba68b130
Matt <matthewpaulh@hotmail.co.uk>
2026-06-28 15:55:55 +0000

    front end serving

--- django_api/django_api/urls.py
+++ django_api/django_api/urls.py
@@ -14,11 +14,10 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from django.http import HttpResponse
+from django.http import HttpResponse, FileResponse
 
 urlpatterns = [
     path('health/', lambda request: HttpResponse(status=204)),
@@ -28,3 +27,15 @@ if settings.STATIC_URL == "/static/":
     urlpatterns.extend(
         static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
     )
+
+def frontend(request):
+    return FileResponse(
+        open(settings.BASE_DIR / "static" / "index.html", "rb"),
+        content_type="text/html",
+    )
+
+frontend_routes = [
+    path("auth/signup/", frontend),
+]
+
+urlpatterns.append(path("", include(frontend_routes)))
